be135fb792cf4498e2fa170ac48fee0fd3ca668c
Stefan Leever <stefan@bcsa.org>
2016-02-07 22:06:50 +0100

    Updated the cachematrix.R file with my submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Library to cache a matrix using a set of two functions of which one will store 
+## the matrix to determine whether it has already been cached and one function to inverse the set matrix.
+## When the matrix has not been cached or the matrix has been changed, the matrix will be inversed and stored in the cache.
 
-## Write a short comment describing this function
+## Created by Stefan Leever @ 2016-02-07
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix(x)
+## This function sets the initial value for the cached matrix (cm), defines the getter and setter functions and returns a list of functions and values.
+## Arguments:
+## x: a square matrix.
+## Return:
+## returns a list with the setter, getter, currently inserted matrix and the original matrix.
 
+makeCacheMatrix <- function (x = matrix()) {
+    cm <- NULL
+    om <<- x
+    set <- function(x) {cm <<- x}
+    get <- function() cm
+    list(set = set, get = get, currentMatrix = x, originalMatrix = om)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(x)
+## This function takes the returned list from makeCacheMatrix and inverses the matrix when it's not cached yet.
+## If either the matrix has not been cached yet OR the matrix is not the same, it will inverse the matrix and store it in the cache if it's a square matrix.
+## Arguments:
+## x: makeCacheMatrix list.
+## Return:
+## returns the inverted matrix.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(x) {
+    matrix <- x$get()
+    if (!is.null(matrix)) {
+        if (identical(x$originalMatrix, x$currentMatrix)) {
+            return(matrix)
+        }
+    }
+    matrix <- solve(x$currentMatrix)
+    x$set(matrix)
+    return (matrix)
 }
+
+# Use the code below to execute the above functions and inverse the matrix.
+x <- makeCacheMatrix(matrix(1:4, ncol=2, nrow=2))
+y <- cacheSolve(x)
